b6616bbdbba3e5fc3c99c93f98f51841cabf4aa6
赵彪 <17354422502@163.com>
2018-10-08 22:04:58 +0800

    edit homework4

--- homework4/program2/Program.cs
+++ homework4/program2/Program.cs
@@ -11,27 +11,25 @@ namespace program2
     {
         static void Main(string[] args)
         {
-            int s = 0;
             Order O1 = new Order("张三", 1, 3, 3, 3);
             Order O2 = new Order("李四", 2, 4, 4, 4);
             List<Order> orderlist = new List<Order>();
             orderlist.Add(O1);
             orderlist.Add(O2);
-            Console.WriteLine("输入进行的操作:1显示所有订单 2添加订单 3删除订单 4查询 5修改订单 6! ");
+            Console.WriteLine("输入进行的操作:1显示所有订单 2添加订单 3删除订单 4查询 5修改订单 6@ ");
             string sss = Console.ReadLine();
-            while (sss !="!")
+            while (sss != "@")
             {
                 int operationNum = int.Parse(sss);
                 switch (operationNum)
                 {
-                    case 1:
+                    case 1:       //利用foreach将所有的订单输出
                         foreach (Order i in orderlist)
                         {
-                            Console.WriteLine(orderlist[s].ClientName + "  订单号：" + orderlist[s].Number + "  苹果的个数" + orderlist[s].AppleNum + "  球的个数" + orderlist[s].BallNum + "  笔的个数" + orderlist[s].PenNum);
-                            s++;
+                            Console.WriteLine(i.ClientName + "  订单号：" + i.Number + "  苹果的个数" + i.AppleNum + "  球的个数" + i.BallNum + "  笔的个数" + i.PenNum);
                         }
-                        break;
-                    case 2:
+                        break;                             
+                    case 2:         //添加订单
                         OrderService order1 = new OrderService();
                         order1.AddOrder();
                         orderlist.Add(order1.list[0]);
@@ -39,18 +37,30 @@ namespace program2
                     case 3:
                         OrderService order2 = new OrderService();
                         order2.list = orderlist;
+                        try
+                        {
+                            Console.WriteLine("输入删除订单号：");
+                            int Searchnum3 = Convert.ToInt32(Console.ReadLine());
+                            Order o32 = order2.SearchOrderNum(Searchnum3);
+                            order2.deleteOrderNum(o32.Number);
+                        }
+                        catch(Exception e)
+                        {
+                            Console.WriteLine("失败了" + e.Message);
+                        }
                         break;
                     case 4:
                         OrderService order4 = new OrderService();
+                        order4.list = orderlist;
                         Console.WriteLine("1客户名称 2订单号");
                         int qq = int.Parse(Console.ReadLine());
                         switch (qq)
                         {
                             case 1:
                                 Console.WriteLine("输入：");
-                                string SearchName = Convert.ToString(Console.ReadLine());
-                                Order o1=order4.SearchOrderName(SearchName);
-                                Console.WriteLine(o1.ClientName + "  订单号：" + o1.Number + "  苹果的个数" + o1.AppleNum + "  球的个数" + o1.BallNum + "  笔的个数" + o1.PenNum);
+                                string SearchName =Console.ReadLine();
+                                Order o41=order4.SearchOrderName(SearchName);
+                                Console.WriteLine(o41.ClientName + "  订单号：" + o41.Number + "  苹果的个数" + o41.AppleNum + "  球的个数" + o41.BallNum + "  笔的个数" + o41.PenNum);
                                 break;
                             case 2:
                                 Console.WriteLine("输入：");
@@ -62,6 +72,7 @@ namespace program2
                         break;
                     case 5:
                         OrderService order3 = new OrderService();
+                        order3.list = orderlist;
                         Console.WriteLine("1修改客户名称 2修改订单号");
                         int qqq = int.Parse(Console.ReadLine());
                         switch (qqq)
@@ -88,10 +99,7 @@ namespace program2
                 Console.WriteLine("输入进行的操作:1显示所有订单 2添加订单 3删除订单 4查询 5修改订单 6! ");
                 sss = Console.ReadLine();
             }
-          
-            
-
-            
+            Environment.Exit(0);
         }
        
     }
@@ -157,10 +165,9 @@ class OrderService
     public Order SearchOrderName(string ClientName)
     {
         Order O1=null;
-        int s = 0;
         foreach(Order i in list)
         {
-            if (list[s].ClientName == ClientName) O1=list[s];
+            if (i.ClientName == ClientName) O1=i;
         }
         return O1;
     }
@@ -168,10 +175,9 @@ class OrderService
     public Order SearchOrderNum(int Number)
     {
         Order O1 = null;
-        int s = 0;
         foreach (Order i in list)
         {
-            if (list[s].Number == Number) O1 = list[s];
+            if (i.Number == Number) O1 = i;
         }
         return O1;
     }
@@ -181,11 +187,12 @@ class OrderService
     {
         try
         {
-            int s = 0;
+            Order ii = null;
             foreach (Order i in list)
             {
-                if (list[s].Number == number) list.Remove(list[s]);
+                if (i.Number == number) ii = i;
             }
+            list.Remove(ii);
         }
         catch (Exception e)
         {
@@ -198,14 +205,13 @@ class OrderService
     //修改客户名称
     public void modifyOrderName(string name)
     {
-        int s = 0;
         try
         {
             Console.Write("输入想要修改的客户名字：");
             string name1 = Convert.ToString(Console.ReadLine());
             foreach (Order i in list)
             {
-                if (list[s].ClientName == name) list[s].ClientName = name1;
+                if (i.ClientName == name) i.ClientName = name1;
             }
         }
         catch (Exception e)
@@ -217,15 +223,13 @@ class OrderService
     //修改客户订单号
     public void modifyOrderNum(string name)
     {
-        int s = 0;
-
         try
         {
             Console.Write("输入订单号：");
             int num = Convert.ToInt32(Console.ReadLine());
             foreach (Order i in list)
             {
-                if (list[s].ClientName == name) list[s].Number = num;
+                if (i.ClientName == name) i.Number = num;
             }
         }
         catch (Exception e)
@@ -237,4 +241,3 @@ class OrderService
     }
 
 }
-
